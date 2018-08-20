4eaca3b8913deead50c99702c543cef818f02772
hulapluto <39414674+hulapluto@users.noreply.github.com>
2018-08-20 08:59:30 -0500

    first commit

---
+++ js/toggle.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  //menu toggle
+  $('.nav-toggle').click(function() {
+    $('.nav').toggleClass('is-open');
+    $('.hamburger').toggleClass('is-open');
+  })
+
+})
